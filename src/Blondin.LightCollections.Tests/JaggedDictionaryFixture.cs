6f9f84f6fdb6f6202e212751bc7f7e805858f850
Thomas Démoulins <tdemoulins@enablon.net>
2016-09-16 08:56:31 +0200

    Fix enumerations issues

--- src/Blondin.LightCollections.Tests/JaggedDictionaryFixture.cs
+++ src/Blondin.LightCollections.Tests/JaggedDictionaryFixture.cs
@@ -129,28 +129,28 @@ namespace Blondin.LightCollections.Tests
             Assert.Equal(value, sut[(IList<int>)key.GetValues()]);
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void SetAndGetIndexedCoordinates_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut[key] = value;
             Assert.Equal(value, sut[key]);
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void SetAndGetIndexedArrayKey_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut[key.GetValues()] = value;
             Assert.Equal(value, sut[key.GetValues()]);
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void SetAndGetIndexedIListKey_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut[(IList<int>)key.GetValues()] = value;
             Assert.Equal(value, sut[(IList<int>)key.GetValues()]);
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void Add_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut.Add(key, value);
@@ -158,27 +158,27 @@ namespace Blondin.LightCollections.Tests
             Assert.Equal(1, sut.Count);
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void NotContainsKey_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             Assert.False(sut.ContainsKey(key));
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void AddAndContainsKey_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut.Add(key, value);
             Assert.True(sut.ContainsKey(key));
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void NotTryGetValue_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             string val;
             Assert.False(sut.TryGetValue(key, out val));
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void AddAndTryGetValue_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut.Add(key, value);
@@ -187,13 +187,13 @@ namespace Blondin.LightCollections.Tests
             Assert.Equal(value, val);
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void NotRemove_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             Assert.False(sut.Remove(key));
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void AddAndRemove_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut.Add(key, value);
@@ -202,14 +202,14 @@ namespace Blondin.LightCollections.Tests
             Assert.Equal(0, sut.Count);
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void Clear_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut.Clear();
             Assert.Equal(0, sut.Count);
         }
 
-        [Theory, CustomAutoData(1)]
+        [Theory, CustomAutoData(5)]
         public void AddAndClear_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int> key, string value)
         {
             sut.Add(key, value);
@@ -217,15 +217,211 @@ namespace Blondin.LightCollections.Tests
             Assert.Equal(0, sut.Count);
         }
 
-        [Theory, CustomAutoData(4)]
-        public void Enumerate(JaggedDictionary<int, string> sut, JaggedIndex4<int>[] keys, string value)
+        [Fact]
+        public void EnumerateBasic_1Dimension()
+        {
+            var indexes = new[] {
+                new[] { 1 },
+                new[] { 2 },
+                new[] { 3 }
+            };
+
+            var sut = new JaggedDictionary<int, int>(indexes.First().Length, SortedDictionaryFactory<int>.Default);
+            var keys = indexes.Select(a => JaggedIndex.Create(a)).ToList();
+            foreach (var key in keys) sut[key] = 0;
+
+            var keyValuePairs = sut.ToList();
+            Assert.Equal(keys.Count, keyValuePairs.Count);
+            foreach (var kvp in keyValuePairs)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(kvp.Key));
+                Assert.Equal(0, kvp.Value);
+            }
+        }
+
+        [Fact]
+        public void EnumerateBasic_2Dimension()
+        {
+            var indexes = new[] {
+                new[] { 1, 1 },
+                new[] { 1, 2 },
+                new[] { 1, 3 },
+                new[] { 2, 1 },
+                new[] { 2, 2 },
+                new[] { 2, 3 },
+                new[] { 3, 1 },
+                new[] { 3, 2 },
+                new[] { 3, 3 }
+            };
+
+            var sut = new JaggedDictionary<int, int>(indexes.First().Length, SortedDictionaryFactory<int>.Default);
+            var keys = indexes.Select(a => JaggedIndex.Create(a)).ToList();
+            foreach (var key in keys) sut[key] = 0;
+
+            var keyValuePairs = sut.ToList();
+            Assert.Equal(keys.Count, keyValuePairs.Count);
+            foreach (var kvp in keyValuePairs)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(kvp.Key));
+                Assert.Equal(0, kvp.Value);
+            }
+        }
+
+        [Fact]
+        public void EnumerateBasic_5Dimension()
+        {
+            var indexes = new[] {
+                new[] { 1, 1, 1, 1, 1 },
+                new[] { 1, 1, 1, 1, 2 },
+                new[] { 1, 1, 1, 1, 3 },
+                new[] { 1, 1, 1, 2, 1 },
+                new[] { 1, 1, 1, 2, 2 },
+                new[] { 1, 1, 1, 2, 3 },
+                new[] { 1, 1, 1, 3, 1 },
+                new[] { 1, 1, 1, 3, 2 },
+                new[] { 1, 1, 1, 3, 3 },
+                new[] { 1, 1, 2, 1, 1 },
+                new[] { 1, 1, 2, 1, 2 },
+                new[] { 1, 1, 2, 1, 3 },
+                new[] { 1, 1, 2, 2, 1 },
+                new[] { 1, 1, 2, 2, 2 },
+                new[] { 1, 1, 2, 2, 3 },
+                new[] { 1, 2, 1, 1, 1 },
+                new[] { 2, 1, 1, 1, 1 },
+                new[] { 2, 2, 2, 2, 2 }
+            };
+
+            var sut = new JaggedDictionary<int, int>(indexes.First().Length, SortedDictionaryFactory<int>.Default);
+            var keys = indexes.Select(a => JaggedIndex.Create(a)).ToList();
+            foreach (var key in keys) sut[key] = 0;
+
+            var keyValuePairs = sut.ToList();
+            Assert.Equal(keys.Count, keyValuePairs.Count);
+            foreach (var kvp in keyValuePairs)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(kvp.Key));
+                Assert.Equal(0, kvp.Value);
+            }
+        }
+
+        [Theory, CustomAutoData(1)]
+        public void Enumerate_1Dimension(JaggedDictionary<int, string> sut, JaggedIndex1<int>[] keys, string value)
         {
             foreach (var key in keys) sut[key] = value;
 
             var keyValuePairs = sut.ToList();
             Assert.Equal(keys.Length, keyValuePairs.Count);
-            foreach (var key in keys)
-                Assert.Contains(key, keyValuePairs.Select(kvp => kvp.Key));
+            foreach (var kvp in keyValuePairs)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(kvp.Key));
+                Assert.Equal(value, kvp.Value);
+            }
+        }
+
+        [Theory, CustomAutoData(2)]
+        public void Enumerate_2Dimension(JaggedDictionary<int, string> sut, JaggedIndex2<int>[] keys, string value)
+        {
+            foreach (var key in keys) sut[key] = value;
+
+            var keyValuePairs = sut.ToList();
+            Assert.Equal(keys.Length, keyValuePairs.Count);
+            foreach (var kvp in keyValuePairs)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(kvp.Key));
+                Assert.Equal(value, kvp.Value);
+            }
+        }
+
+        [Theory, CustomAutoData(5)]
+        public void Enumerate_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int>[] keys, string value)
+        {
+            foreach (var key in keys) sut[key] = value;
+
+            var keyValuePairs = sut.ToList();
+            Assert.Equal(keys.Length, keyValuePairs.Count);
+            foreach (var kvp in keyValuePairs)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(kvp.Key));
+                Assert.Equal(value, kvp.Value);
+            }
+        }
+
+        [Theory, CustomAutoData(1)]
+        public void Keys_1Dimension(JaggedDictionary<int, string> sut, JaggedIndex1<int>[] keys, string value)
+        {
+            foreach (var key in keys) sut[key] = value;
+
+            var list = sut.Keys.ToList();
+            Assert.Equal(keys.Length, list.Count);
+            foreach (var item in list)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(item));
+            }
+        }
+
+        [Theory, CustomAutoData(2)]
+        public void Keys_2Dimension(JaggedDictionary<int, string> sut, JaggedIndex2<int>[] keys, string value)
+        {
+            foreach (var key in keys) sut[key] = value;
+
+            var list = sut.Keys.ToList();
+            Assert.Equal(keys.Length, list.Count);
+            foreach (var item in list)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(item));
+            }
+        }
+
+        [Theory, CustomAutoData(5)]
+        public void Keys_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int>[] keys, string value)
+        {
+            foreach (var key in keys) sut[key] = value;
+
+            var list = sut.Keys.ToList();
+            Assert.Equal(keys.Length, list.Count);
+            foreach (var item in list)
+            {
+                Assert.True(keys.Cast<IJaggedIndex<int>>().Contains(item));
+            }
+        }
+
+        [Theory, CustomAutoData(1)]
+        public void Values_1Dimension(JaggedDictionary<int, string> sut, JaggedIndex1<int>[] keys, string value)
+        {
+            foreach (var key in keys) sut[key] = value;
+
+            var list = sut.Values.ToList();
+            Assert.Equal(keys.Length, list.Count);
+            foreach (var item in list)
+            {
+                Assert.Equal(value, item);
+            }
+        }
+
+        [Theory, CustomAutoData(2)]
+        public void Values_2Dimension(JaggedDictionary<int, string> sut, JaggedIndex2<int>[] keys, string value)
+        {
+            foreach (var key in keys) sut[key] = value;
+
+            var list = sut.Values.ToList();
+            Assert.Equal(keys.Length, list.Count);
+            foreach (var item in list)
+            {
+                Assert.Equal(value, item);
+            }
+        }
+
+        [Theory, CustomAutoData(5)]
+        public void Values_5Dimension(JaggedDictionary<int, string> sut, JaggedIndex5<int>[] keys, string value)
+        {
+            foreach (var key in keys) sut[key] = value;
+
+            var list = sut.Values.ToList();
+            Assert.Equal(keys.Length, list.Count);
+            foreach (var item in list)
+            {
+                Assert.Equal(value, item);
+            }
         }
     }
 }
